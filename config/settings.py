da8f71fd8d24fa8d27c25e63522f0cba49d54e69
JKodes <adrainford09@gmail.com>
2024-04-25 16:22:38 -0400

    add configuration tailwind third party install

--- config/settings.py
+++ config/settings.py
@@ -38,6 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     
+    #3rd part installs
+    'crispy_forms',
+    'crispy_tailwind',
+    
     #local app name
     'linksrepo'
 ]
@@ -124,3 +128,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# Crispy Settings
+CRISPY_ALLOWED_TEMPLATE_PACKS = "tailwind"
+
+CRISPY_TEMPLATE_PACK = "tailwind"
