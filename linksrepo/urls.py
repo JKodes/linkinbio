b9ccd5e8b0a4784932e243cf0b0d16b98ce9b2ff
JKodes <adrainford09@gmail.com>
2024-04-25 17:01:03 -0400

    add a path to update link

--- linksrepo/urls.py
+++ linksrepo/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
-from .views import LinkListView, LinkCreateView
+from .views import LinkListView, LinkCreateView, LinkUpdateView
 
 urlpatterns = [
     path('', LinkListView.as_view(), name="links-list"),
     path('link/create', LinkCreateView.as_view(), name="links-create"),
+    path('link/<int:pk>/update', LinkUpdateView.as_view(), name="links-update"),
+    
 ]
