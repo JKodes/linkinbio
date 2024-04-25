c954f85d512d1203c241bfa50dc281b89679ec6b
JKodes <adrainford09@gmail.com>
2024-04-25 15:57:33 -0400

    add path to the create new links

--- linksrepo/urls.py
+++ linksrepo/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import LinkListView
+from .views import LinkListView, LinkCreateView
 
 urlpatterns = [
     path('', LinkListView.as_view(), name="links-list"),
+    path('link/create', LinkCreateView.as_view(), name="links-create"),
 ]
