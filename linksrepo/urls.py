53fa69f97259e562dc5922d34ae10d607cd82dd7
JKodes <adrainford09@gmail.com>
2024-04-26 16:31:20 -0400

    create path for profile view

--- linksrepo/urls.py
+++ linksrepo/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
-from .views import LinkListView, LinkCreateView, LinkUpdateView, LinkDeleteView
+from .views import LinkListView, LinkCreateView, LinkUpdateView, LinkDeleteView, profile_view
 
 urlpatterns = [
     path('', LinkListView.as_view(), name="links-list"),
     path('link/create', LinkCreateView.as_view(), name="links-create"),
     path('link/<int:pk>/update', LinkUpdateView.as_view(), name="links-update"),
     path('link/<int:pk>/delete', LinkDeleteView.as_view(), name="links-delete"),
+    path('<slug:profile_slug>/', profile_view, name="profile"),
     
 ]
